729a0fd376439ffb751643f0ddd2dc85312f8e27
Obichigbo <obicnwokolo@gmail.com>
2024-11-27 16:18:47 +0000

    Update1

---
+++ src/morechallenge.py
@@ -0,0 +1,13 @@
+# IF Statements
+n = int(input("please enter your score: "))
+if n == 0:
+    print('Fail')
+elif n <50:
+    print("Resit")
+elif n < 70:
+    print('Pass')
+else:
+    print("Promote & Reward")
+
+
+
