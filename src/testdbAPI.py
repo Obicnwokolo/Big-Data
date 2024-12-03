2ee2d29c63e5c8f9778479eac20a6f89ed47285f
Obicnwokolo <nwokolo.obinna10@gmail.com>
2024-12-03 16:44:19 +0000

    update12

--- src/testdbAPI.py
+++ src/testdbAPI.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 # importing connection engine pack
 from sqlalchemy import create_engine, inspect, text
-#from urllib.parse import quote_plus #why
 from sqlalchemy import Table, MetaData
 
 # Creating connection strings for my database
@@ -13,33 +12,12 @@ password = "WelcomeItc@2022"
 host= "18.132.73.146"
 port = "5432"
 database= "testdb"
-#ENCODED_PASSWORD = quote_plus(password)
 
 
 #creating database connectionw string
-#connection_string = "postgresql+psycopg2://{username}:{ENCODED_PASSWORD}@{host}:{port}/{database}"
+
 engine = create_engine('postgresql://consultants:WelcomeItc%402022@18.132.73.146:5432/testdb')
-#Establishing connection with engine & database
-#engine = create_engine(connection_string)
-#cursor = connection_string.cursor()
 
-#try:
-   # with engine.connect() as connection: #explain???
-     #   print("connection successful")
-#except Exception as e:  #explain????
- #   print("connection failed:", e)
-
-# Read csv from python as data frame and load it to sql
-#data_load = pd.read_csv("C:/Users/chigb/Downloads/5_fraud_records.csv")
-
-# load dataframe to sql
-#try:
-   # data_load.to_sql('fraud_table',con=engine, if_exists= 'replace', index= False)
-   # print("Data successfully added to database")
-#except Exception as e:
-   # print("An error occored: {e}")
-
-#connection.commit()
 
 fraud_data = pd.read_sql("fraud_table",engine)
 
